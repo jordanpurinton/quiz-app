0f1120ecfb54d10332ff1d2937006ec14c669457
MDurrin <MDurrin@DESKTOP-GBEN3E9>
2017-04-07 10:38:08 -0600

    Added a redirect to account management on the Login page if the user is already logged in.

--- QuizMania_v2/Bootstrap_Test/Account/Login.aspx.cs
+++ QuizMania_v2/Bootstrap_Test/Account/Login.aspx.cs
@@ -12,6 +12,11 @@ namespace Bootstrap_Test.Account
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Page.User.Identity.IsAuthenticated)
+            {
+                Response.Redirect("Account/Manage.aspx");
+            }
+
             RegisterHyperLink.NavigateUrl = "Register";
             // Enable this once you have account confirmation enabled for password reset functionality
             //ForgotPasswordHyperLink.NavigateUrl = "Forgot";
