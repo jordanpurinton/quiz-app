ef422bbe02b4593c7bd7e4ede0678693cf35c05c
bsimmt <blake.simmel@umconnect.umt.edu>
2017-04-10 10:33:24 -0600

    fixed login pointing to wrong missing manage page

--- QuizMania_v2/Bootstrap_Test/Account/Login.aspx.cs
+++ QuizMania_v2/Bootstrap_Test/Account/Login.aspx.cs
@@ -12,11 +12,12 @@ namespace Bootstrap_Test.Account
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            
             if (Page.User.Identity.IsAuthenticated)
             {
-                Response.Redirect("Account/Manage.aspx");
+                Response.Redirect("Manage.aspx");
             }
-
+            
             RegisterHyperLink.NavigateUrl = "Register";
             // Enable this once you have account confirmation enabled for password reset functionality
             //ForgotPasswordHyperLink.NavigateUrl = "Forgot";
